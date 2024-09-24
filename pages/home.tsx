4d42c2e4bbfde690c9ababde3809c7171a723173
kongo-28 <ryota.tennis.ku@gmail.com>
2024-09-24 19:41:45 +0900

    ログアウト後リロードしhomeに戻るよう修正

--- pages/home.tsx
+++ pages/home.tsx
@@ -9,30 +9,34 @@ import { useRouter } from "next/router";
 
 export const getServerSideProps: GetServerSideProps =
   withAuthServerSideProps("/api/v1/home");
-//////////////////ログアウト機能/////////////
-const handleLogout = async () => {
-  try {
-    await axios.delete("http://localhost:3000/api/v1/auth/sign_out", {
-      headers: {
-        "Content-Type": "application/json",
-        uid: Cookies.get("uid"),
-        client: Cookies.get("client"),
-        "access-token": Cookies.get("access-token"),
-      },
-    });
-  } catch (err) {
-    alert("削除に失敗しました");
-  }
-};
-//////////////////ログアウト機能/////////////
+
 const Home = () => {
+  const router = useRouter();
+  //////////////////ログアウト機能/////////////
+  const handleLogout = async () => {
+    console.log("aaaa");
+    try {
+      await axios.delete("http://localhost:3000/api/v1/auth/sign_out", {
+        headers: {
+          uid: Cookies.get("uid"),
+          client: Cookies.get("client"),
+          "access-token": Cookies.get("access-token"),
+        },
+      });
+      router.reload();
+    } catch (err) {
+      alert("削除に失敗しました");
+    }
+  };
+  //////////////////ログアウト機能/////////////
+
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}>
           <h1 className={styles.title}>HOME</h1>
           <p className={styles.description}>ホーム画面です</p>{" "}
-          <button onClick={() => handleLogout}>Logout</button>
+          <button onClick={handleLogout}>Logout</button>
         </main>
       </div>
     </>
